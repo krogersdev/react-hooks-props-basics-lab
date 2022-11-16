b0dac765ec2a8353a04ca3673fb16dabc39226ef
Kevin Rogers <kevinarogers1@gmail.com>
2022-11-15 21:20:34 -0500

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Links from "./Links";
 
-function About({ bio,links }) {
-   
+function About({ bio, links: { github, linkedin } }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      { !bio ? null: <p> {bio} </p> }
+      {!bio ? null : <p> {bio} </p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={links.github} linkedin={links.linkedin}/> 
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
